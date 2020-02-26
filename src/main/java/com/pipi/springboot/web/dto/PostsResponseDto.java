2273f8baac709aa82104c128177b12465af7049e
[kjy200] <[rlawnduf200@naver.com]>
2020-02-26 23:23:16 +0900

    뭔가 많이 수정 하지만 ㅈ됨

---
+++ src/main/java/com/pipi/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package com.pipi.springboot.web.dto;
+
+
+import com.pipi.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getTitle();
+        this.author = entity.getAuthor();
+    }
+}
